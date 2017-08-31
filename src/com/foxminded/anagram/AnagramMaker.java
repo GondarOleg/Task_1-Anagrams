8a8b78750aa7dc78dff116958829e2d6e8226a4e
OMG <_oleg@ua.fm>
2017-08-31 18:35:51 +0300

    Code refactored

--- src/com/foxminded/anagram/AnagramMaker.java
+++ src/com/foxminded/anagram/AnagramMaker.java
@@ -1,20 +1,33 @@
 package com.foxminded.anagram;
 
 public class AnagramMaker {
-    public static String makeAnagram(String string) {
 
+    public static String makeAnagram(String string) {
+        String anagram = "";
         if (string.length() > 1) {
-            String[] wordsArr = string.split(" ");
-            string = "";
-            for (String s : wordsArr
-                    ) {
-                String tempString = "";
-                for (int i = s.length() - 1; i >= 0; i--) {
-                    tempString += s.charAt(i);
+            String tempString = "";
+            for (int i = 0; i < string.length(); i++) {
+                if (Character.isLetter(string.charAt(i))) {
+                    tempString += string.charAt(i);
+                } else {
+                    tempString = swapCharsInString(tempString);
+                    anagram += tempString + string.charAt(i);
+                    tempString = "";
                 }
-                string += tempString + " ";
             }
+            if (!tempString.isEmpty()) {
+                anagram += swapCharsInString(tempString);
+            }
+            return anagram;
         }
         return string;
+    }
+
+    public static String swapCharsInString(String string) {
+        String tempString = "";
+        for (int i = string.length() - 1; i >= 0; i--) {
+            tempString += string.charAt(i);
+        }
+        return tempString;
     }
 }
